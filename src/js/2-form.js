bcf58c2ea5e5339e65df8beaee8977f4ec7ee50c
Zexler <zexler@gmail.com>
2026-05-20 23:59:49 +0200

    Init

---
+++ src/js/2-form.js
@@ -0,0 +1,34 @@
+const form = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+const savedData = localStorage.getItem(localStorageKey);
+if (savedData) {
+  formData = JSON.parse(savedData);
+  form.elements.email.value = formData.email || '';
+  form.elements.message.value = formData.message || '';
+}
+
+form.addEventListener('input', event => {
+  formData[event.target.name] = event.target.value.trim();
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(localStorageKey);
+  formData = { email: '', message: '' };
+  form.reset();
+});
